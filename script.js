04226b801b347b8de7e5e1b5025375f8626ca6c9
Richard Mariano <97739303+RichardMariano@users.noreply.github.com>
2022-09-01 19:27:01 -0300

    commit inicial
    Primeiro commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function menu() {
+    var mobile = document.querySelector('.menu-mobile')
+    var menu = document.querySelector('.toggle')
+
+    mobile.classList.toggle('active')
+    menu.classList.toggle('active')
+}
